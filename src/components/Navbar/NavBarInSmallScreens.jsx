54e1eaadf9c34d18f0088e839ec171ae047115a6
Omar-Abosea3 <oa615010@gmail.com>
2024-07-06 14:23:07 +0300

    new edits

--- src/components/Navbar/NavBarInSmallScreens.jsx
+++ src/components/Navbar/NavBarInSmallScreens.jsx
@@ -14,22 +14,22 @@ export default function NavBarInSmallScreens({
         <Navbar className="d-flex justify-content-center align-items-center">
           <Nav>
             <Nav.Item eventKey="1" title="home">
-              <Link  to={'/home'} className='nav-link'>
+              <Link  to={'/home'} className='text-decoration-none text-black'>
                 <i className="bi fs-4 w-100 text-center bi-house-door"></i>
               </Link>
             </Nav.Item>
             <Nav.Item eventKey="2" title="brands">
-              <Link  to={'/brands'} className='nav-link'>
+              <Link  to={'/brands'} className='text-decoration-none text-black'>
                 <i className="bi text-center w-100 fs-4 bi-shop-window position-relative"></i>
               </Link>
             </Nav.Item>
             <Nav.Item eventKey="3" title="Logo">
-                <Link to={'/'} className='text-decoration-none'>
+                <Link to={'/'} className='text-decoration-none text-black'>
                     <div className='navBarLogoInSmallScreen'>T</div>
                 </Link>
             </Nav.Item>
             <Nav.Item eventKey="3" title="cart">
-                <Link  to={'/cart'} className='nav-link'>
+                <Link  to={'/cart'} className='text-decoration-none text-black'>
                     <i className="bi text-center w-100 fs-4 bi-cart position-relative">
                         {curUser && <div className="favBrandCounter position-absolute bottom-50 start-50">
                             <span className="fs-6">{numOfCartItems}</span>
@@ -38,7 +38,7 @@ export default function NavBarInSmallScreens({
                 </Link>
             </Nav.Item>
             {curUser && <Nav.Item eventKey="3" title="wishlist">
-                <Link  to={'/wishlist'} className='nav-link'>
+                <Link  to={'/wishlist'} className='text-decoration-none text-black'>
                     <i className="bi text-center w-100 fs-4 bi-heart position-relative">
                         <div
                           style={{ left: "50%" }}
@@ -53,7 +53,7 @@ export default function NavBarInSmallScreens({
               eventKey="3"
               title={curUser ? "profile" : "login"}
             >
-              <Link  to={curUser?'/profile':'/login'} className='nav-link'><i className="bi bi-person fs-4"></i></Link>
+              <Link  to={curUser?'/profile':'/login'} className='text-decoration-none text-black'><i className="bi bi-person fs-4"></i></Link>
             </Nav.Item>
           </Nav>
         </Navbar>
